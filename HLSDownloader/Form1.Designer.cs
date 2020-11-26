1de1e0a8a177538f9f847ae58a0a01e8172774af
Mustafa Ulukaya <mustafaulukaya@outlook.com.tr>
2020-11-26 22:37:19 +0300

    first commit

---
+++ HLSDownloader/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace HLSDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.mediaUrlTextBox = new System.Windows.Forms.TextBox();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.downloadPercent = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.threadCountNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.threadCountLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.threadCountNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // mediaUrlTextBox
+            // 
+            this.mediaUrlTextBox.Location = new System.Drawing.Point(92, 32);
+            this.mediaUrlTextBox.Name = "mediaUrlTextBox";
+            this.mediaUrlTextBox.Size = new System.Drawing.Size(606, 20);
+            this.mediaUrlTextBox.TabIndex = 1;
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(262, 223);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 2;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(92, 68);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(606, 23);
+            this.progressBar.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Status";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.statusLabel.Location = new System.Drawing.Point(98, 119);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(31, 13);
+            this.statusLabel.TabIndex = 5;
+            this.statusLabel.Text = "IDLE";
+            // 
+            // downloadPercent
+            // 
+            this.downloadPercent.AutoSize = true;
+            this.downloadPercent.Location = new System.Drawing.Point(704, 73);
+            this.downloadPercent.Name = "downloadPercent";
+            this.downloadPercent.Size = new System.Drawing.Size(0, 13);
+            this.downloadPercent.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Eş zamanlı indirme sayısı";
+            // 
+            // threadCountNumericUpDown
+            // 
+            this.threadCountNumericUpDown.Location = new System.Drawing.Point(181, 155);
+            this.threadCountNumericUpDown.Name = "threadCountNumericUpDown";
+            this.threadCountNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.threadCountNumericUpDown.TabIndex = 10;
+            this.threadCountNumericUpDown.UseWaitCursor = true;
+            this.threadCountNumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(410, 223);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 11;
+            this.resetButton.Text = "Sıfırla";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Thread Sayısı";
+            // 
+            // threadCountLabel
+            // 
+            this.threadCountLabel.AutoSize = true;
+            this.threadCountLabel.Location = new System.Drawing.Point(152, 183);
+            this.threadCountLabel.Name = "threadCountLabel";
+            this.threadCountLabel.Size = new System.Drawing.Size(13, 13);
+            this.threadCountLabel.TabIndex = 13;
+            this.threadCountLabel.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 280);
+            this.Controls.Add(this.threadCountLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.threadCountNumericUpDown);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.downloadPercent);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.mediaUrlTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.threadCountNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox mediaUrlTextBox;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label downloadPercent;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown threadCountNumericUpDown;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label threadCountLabel;
+    }
+}
+
---
+++ HLSDownloader/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HLSDownloader
+{
+    public partial class Form1 : Form
+    {
+        private List<Segment> segments = new List<Segment>();
+        private Dictionary<int, byte[]> Datas=new Dictionary<int, byte[]>();
+        private static List<Thread> threads = new List<Thread>();
+        private WebClient client;
+        private string baseUrl;
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+            client = new WebClient();
+        }
+
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SetStatusLabel("STARTED");
+                var mediaUrl = mediaUrlTextBox.Text;
+                var splittedMediUrl = mediaUrl.Split('/');
+
+                for (var i = 0; i < splittedMediUrl.Length - 1; i++)
+                {
+                    var s = splittedMediUrl[i];
+                    baseUrl += s + "/";
+                }
+
+                var manifest = client.DownloadString(mediaUrl);
+
+                var regex = new Regex("#EXTINF:.*");
+
+                var tempSegments = regex.Split(manifest).ToList();
+                tempSegments.RemoveAt(0);
+
+                var index = 0;
+                foreach (var tempSegment in tempSegments)
+                {
+                    var segment = tempSegment.Replace("\n", "");
+                    segments.Add(new Segment()
+                    {
+                        Index = index,
+                        Url = segment
+                    });
+
+                    index++;
+                }
+
+
+                progressBar.Minimum = 0;
+                progressBar.Maximum = segments.Count;
+                progressBar.Step = 1;
+
+                if (threadCountNumericUpDown.Value <= 0)
+                {
+                    MessageBox.Show("Thread Sayısı 0 dan az veya 10 dan fazla olamaz", "Hata",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (threadCountNumericUpDown.Value > segments.Count)
+                        threadCountNumericUpDown.Value = segments.Count;
+
+                    for (var i = 0; i < threadCountNumericUpDown.Value; i++)
+                    {
+                        DownloadData();
+                    }
+                }
+                
+            }
+            catch (Exception)
+            {
+                SetStatusLabel("ERROR");
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        private void SetStatusLabel(string status)
+        {
+            statusLabel.Text = status;
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        private void SetPercentLabel(double percent)
+        {
+            downloadPercent.Text = "%" + percent.ToString();
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        private Segment GetSegment()
+        {
+            var segment = segments.First(op => !op.IsDownloadStarted && !op.IsFinished);
+            segments.Remove(segment);
+            segment.IsDownloadStarted = true;
+            segments.Add(segment);
+            return segment;
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        private void SetSegmentCompleted(Segment segment)
+        {
+            var _segment = segments.First(op => op.Index == segment.Index);
+            segments.Remove(_segment);
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        private void PerformStep()
+        {
+            progressBar.PerformStep();
+        }
+
+        private void DownloadData()
+        {
+            Thread bgThread = new Thread(() =>
+            {
+                try
+                {
+                    var segment = GetSegment();
+                    do
+                    {
+                        if (segment != null)
+                        {
+                            using (WebClient client = new WebClient())
+                            {
+                                var segmentFileUrl = baseUrl + segment.Url;
+                                var tempData = client.DownloadData(segmentFileUrl);
+                                Datas.Add(segment.Index, tempData);
+                                PerformStep();
+                                CalculatePercent();
+                                SetSegmentCompleted(segment);
+                            }
+
+                            segment = GetSegment();
+                        }
+
+                    } while (segment != null);
+                }
+                catch (Exception ex)
+                {
+                    SetStatusLabel("ERROR (" + ex.Message + ")");
+                }
+            });
+            threads.Add(bgThread);
+            bgThread.Start();
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        private void CalculatePercent(bool isFileUnion = false)
+        {
+            threadCountNumericUpDown.Value = threads.Count;
+            double value = (100 * progressBar.Value) / progressBar.Maximum;
+            SetPercentLabel(value);
+            if (progressBar.Value == progressBar.Maximum && !isFileUnion)
+                DownloadCompleted();
+        }
+
+        private void DownloadCompleted()
+        {
+            var index = 0;
+            progressBar.Value = 0;
+            List<byte> file = new List<byte>();
+            var filePieceCount = Datas.Count;
+            foreach (var data in Datas)
+            {
+                SetStatusLabel("Parçalar Birleştiriliyor.. " + index + "/" + filePieceCount);
+                byte[] value;
+                _ = Datas.TryGetValue(index, out value);
+                file = file.Concat(value).ToList();
+                index++;
+                PerformStep();
+                CalculatePercent(true);
+            }
+
+            var path = Directory.GetCurrentDirectory() + "/video.ts";
+
+            try
+            {
+                File.WriteAllBytes(path, file.ToArray());
+                SetStatusLabel("FINISHED & SAVED");
+                MessageBox.Show($"Dosya {path} yoluna kaydedildi.", "Bilgi", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Dosya diske kaydedilirken hata oluştu.", "Hata", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+
+            finally
+            {
+                progressBar.Value = 0;
+                segments.Clear();
+                Datas.Clear();
+                foreach (var thread in threads)
+                {
+                    thread.Abort();
+                }
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            threadCountNumericUpDown.Value = 1;
+            mediaUrlTextBox.Text = "";
+            progressBar.Value = 0;
+            segments.Clear();
+            Datas.Clear();
+            foreach (var thread in threads)
+            {
+                thread.Abort();
+            }
+        }
+    }
+
+    public class Segment
+    {
+        public string Url { get; set; }
+        public int Index { get; set; }
+        public bool IsDownloadStarted { get; set; } = false;
+        public bool IsFinished { get; set; } = false;
+    }
+}
